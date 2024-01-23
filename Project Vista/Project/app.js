62d2fc6ab9e4def3a6834db4ce2fa28fa7de4706
PurinSiripru <122721492+PurinSiripru@users.noreply.github.com>
2024-01-23 10:56:08 +0700

    app.js

--- Project Vista/Project/app.js
+++ Project Vista/Project/app.js
@@ -1,8 +1,7 @@
-const API_KEY = "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"
+const API_KEY = "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"
 
 const submitIcon = document.querySelector("#submit-icon")
 const inputElement = document.querySelector("input")
-const imageSection = document.querySelector(".images-section")
 const getImages = async () => {
     const options = 
     {
@@ -20,18 +19,10 @@ const getImages = async () => {
     try {
         const response = await fetch('https://api.openai.com/v1/images/generations' , options)
         const data = await response.json()
-
-        data?.data.array.forEach(imageObject =>{
-        const ImageContainer= document.createElement("div")
-        ImageContainer.classList.add('image-container')
-        const imageElement= document.createElement("img") 
-        imageElement.setAttribute('src', imageObject.url)
-        ImageContainer.append(imageElement)
-        imageSection.append(ImageContainer)
-        })
+        console.log(data)
 
     } catch (error) {
-        
+        console.log(error)
     }
 }
 submitIcon.addEventListener('click', getImages)
