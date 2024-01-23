9a374c6833c91b4f1261b77846d6ae101cc3b065
PurinSiripru <122721492+PurinSiripru@users.noreply.github.com>
2024-01-23 13:16:48 +0700

    app.js

--- Project Vista/Project/app.js
+++ Project Vista/Project/app.js
@@ -1,7 +1,9 @@
-const API_KEY = "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"
+const API_KEY = "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"
 
 const submitIcon = document.querySelector("#submit-icon")
 const inputElement = document.querySelector("input")
+const imageSection = document.querySelector('.images-section')
+
 const getImages = async () => {
     const options = 
     {
@@ -19,7 +21,16 @@ const getImages = async () => {
     try {
         const response = await fetch('https://api.openai.com/v1/images/generations' , options)
         const data = await response.json()
-        console.log(data)
+        data?.data.forEach(imageObject => {
+            const ImageContainer = document.createElement('div')
+            ImageContainer.classList.add('image-container')
+            const imageElement = document.createElement('img')
+            imageElement.setAttribute('src', imageObject.url)
+            ImageContainer.append(imageElement)
+            imageSection.append(ImageContainer)
+
+
+        });
 
     } catch (error) {
         console.log(error)
