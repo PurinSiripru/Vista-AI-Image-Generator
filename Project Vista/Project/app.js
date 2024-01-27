4318ff59d485b1172a39117a163bcb7b2efd5920
PurinSiripru <122721492+PurinSiripru@users.noreply.github.com>
2024-01-27 16:07:28 +0700

    app.js

--- Project Vista/Project/app.js
+++ Project Vista/Project/app.js
@@ -1,39 +1,73 @@
-const API_KEY = "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"
+const generateForm = document.querySelector(".generate-form");
+const imageGallery = document.querySelector(".image-gallery");
 
-const submitIcon = document.querySelector("#submit-icon")
-const inputElement = document.querySelector("input")
-const imageSection = document.querySelector('.images-section')
+const OPENAI_API_KEY = "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";
+let isImageGenerating = false;
 
-const getImages = async () => {
-    const options = 
-    {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${API_KEY}`,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            prompt: inputElement.value,
-            n: 4,
-            size:"1024x1024"
-        })
-    }
+const updateImageCard = (imgDataArray) => {
+    imgDataArray.forEach((imgObject, index) => {
+        const imgCard = imageGallery.querySelectorAll(".img-card")[index];
+        const imgElement = imgCard.querySelector("img");
+        const downloadBtn = imgCard.querySelector(".download-btn");
+
+        const AIGenerateImg = `data:image/jpeg;base64,${imgObject.b64_json}`;
+        imgElement.src = AIGenerateImg;
+
+        imgElement.onload= () => {
+            imgCard.classList .remove("loading");
+            downloadBtn.setAttribute("href" , AIGenerateImg);
+            downloadBtn.setAttribute("download" , `${new Date().getTime()}.jpg`);
+
+        }
+    });
+}
+
+const generateAIImages = async (userPrompt , userImgQuantity) => {
     try {
-        const response = await fetch("https://api.openai.com/v1/images/generations" , options)
-        const data = await response.json()
-        data?.data.forEach(imageObject => {
-            const imageContainer = document.createElement("div")
-            imageContainer.classList.add("image-container")
-            const imageElement = document.createElement("img")
-            imageElement.setAttribute("src", imageObject.url)
-            imageContainer.append(imageElement)
-            imageSection.append(imageContainer)
-
-
+        const response = await fetch("https://api.openai.com/v1/images/generations" ,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                prompt: userPrompt,
+                n: parseInt(userImgQuantity),
+                size: "512x512",
+                response_format: "b64_json"
+            })
         });
 
+        if(!response.ok) throw new Error("Failed to generate images! Please try again.")
+
+        const {data} = await response.json();
+        updateImageCard([...data]);
     } catch (error) {
-        console.log(error)
+        alert(error.message);
+    } finally{
+        return isImageGenerating = false;
     }
 }
-submitIcon.addEventListener('click', getImages)
+
+const handleFormSubmission = (e) => {
+    e.preventDefault();
+    if (isImageGenerating)
+        return isImageGenerating = true;
+    
+    const userPrompt = e.srcElement[0].value;
+    const userImgQuantity = e.srcElement[1].value;
+
+    const imgCardMarkup = Array.from({length: userImgQuantity} , () =>
+        `<div class ="img-card loading">
+            <img src = "images/loader.svg" alt="image">
+            <a href="#" class="download-btn">
+                <img src = "images/download.svg" alt = "download icon">
+            </a>
+        </div>`
+    ).join("");
+
+    imageGallery.innerHTML = imgCardMarkup;
+    generateAIImages(userPrompt , userImgQuantity);
+}
+
+generateForm.addEventListener("submit" , handleFormSubmission);
